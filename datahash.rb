0d2b2179728b2e89bd018c97803aa5dca757b3d5
Victor <valemany@shutterstock.com>
2014-09-17 16:25:52 -0400

    How to load data from different data structures with ruby

---
+++ datahash.rb
@@ -0,0 +1,23 @@
+ DATA = {
+    :admin => {
+      :url => 'http://admin.shutterstock.com',
+      :dev => {
+        :db_host => 'dbmaster.dev.shuttercorp.net',
+        :url => 'http://admin.integration.dev.shutterstock.com',
+      },
+      :qa => {
+        :db_host => 'dbmaster.qa.shuttercorp.net',
+      },
+    },
+    :api => {
+      :url => 'http://api.shutterstock.com',
+      :user => 'ntf',
+      :key => '259eeb5b36f6547abaaf9a2c80ae48a5aa715360',
+      :dev => {
+        :key => '259eeb5b36f6547abaaf9a2c80ae48a5aa715360',
+        :url => 'http://api.integration.dev.shutterstock.com',
+      },
+      :qa => {
+        :key => 'c861e797564a8b524e23b1efd9d77b37ea139f90',
+      }
+    }}
