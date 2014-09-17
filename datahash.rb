ee3946a697e37b9a7907510f0d24e91b00afee77
Victor <valemany@shutterstock.com>
2014-09-17 16:35:42 -0400

    removing more unused data

--- datahash.rb
+++ datahash.rb
@@ -1,23 +1,23 @@
  DATA = {
     :admin => {
-      :url => 'http://admin.shutterstock.com',
+      :url => 'http://dsfkjhdskfj.com',
       :dev => {
-        :db_host => 'dbmaster.dev.shuttercorp.net',
-        :url => 'http://admin.integration.dev.shutterstock.com',
+        :db_host => 'hello.net',
+        :url => 'http://whatever.com',
       },
       :qa => {
-        :db_host => 'dbmaster.qa.shuttercorp.net',
+        :db_host => 'yes.net',
       },
     },
     :api => {
       :url => 'http://api.shutterstock.com',
       :user => 'ntf',
-      :key => '259eeb5b36f6547abaaf9a2c80ae48a5aa715360',
+      :key => 'alsdjfhdskfjhdkjfh',
       :dev => {
-        :key => '259eeb5b36f6547abaaf9a2c80ae48a5aa715360',
-        :url => 'http://api.integration.dev.shutterstock.com',
+        :key => 'dsfjkhdsfkjdshfkjdsf',
+        :url => 'dslkfjhdlfjkhdskjfhdksjh',
       },
       :qa => {
-        :key => 'c861e797564a8b524e23b1efd9d77b37ea139f90',
+        :key => 'dsljkfhdfkjhdskjfhdskjfhdskj',
       }
     }}
